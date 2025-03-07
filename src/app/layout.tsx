3c5697f23202ac48b2fef5cc5abe041efeaa9d78
w3barsi <darcybalaga@gmail.com>
2025-03-07 21:19:35 +0800

    removed toaster closeButton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster position="top-center" richColors closeButton theme="light" />
+        <Toaster position="top-center" richColors theme="light" />
         {children}
       </body>
     </html>
